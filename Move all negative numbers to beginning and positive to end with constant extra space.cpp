926bf1831f39eb197797e38e2f768049795bc5d6
Vivek Kumar Srivastava <43187055+Viv786ek@users.noreply.github.com>
2021-04-02 15:43:55 +0530

    Update Move all negative numbers to beginning and positive to end with constant extra space.cpp

--- Move all negative numbers to beginning and positive to end with constant extra space.cpp
+++ Move all negative numbers to beginning and positive to end with constant extra space.cpp
@@ -17,6 +17,46 @@ void printArray(int arr[],int n){
         cout<<arr[i]<<" ";
     }
 }
+// 2nd Method
+/*
+// negative elements on left side
+void shiftall(int arr[], int left,
+              int right)
+{
+   
+  // Loop to iterate over the
+  // array from left to the right
+  while (left<=right)
+  {
+    // Condition to check if the left
+    // and the right elements are
+    // negative
+    if (arr[left] < 0 && arr[right] < 0)
+      left+=1;
+     
+    // Condition to check if the left
+    // pointer element is positive and
+    // the right pointer element is negative
+    else if (arr[left]>0 && arr[right]<0)
+    {
+      int temp=arr[left];
+      arr[left]=arr[right];
+      arr[right]=temp;
+      left+=1;
+      right-=1;
+    }
+     
+    // Condition to check if both the
+    // elements are positive
+    else if (arr[left]>0 && arr[right] >0)
+      right-=1;
+    else{
+      left += 1;
+      right -= 1;
+    }
+  }
+}
+*/
 
 int main()
 {
